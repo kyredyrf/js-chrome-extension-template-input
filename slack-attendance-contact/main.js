8de9a8f4f4ea9e64e3c466415b31a8b7869b23ca
kyredyrf <xs4_22dc7932_github@yahoo.co.jp>
2021-02-07 10:31:31 +0900

    Chatwork&Slack用勤怠入力支援

---
+++ slack-attendance-contact/main.js
@@ -0,0 +1,42 @@
+document.onkeydown = function (e) {
+    function kCurrent() { return 1 }
+    function kForward() { return 2 }
+    function kBackward() { return 3 }
+
+    function input(text, direction) {
+        // UI 変わったらこの辺を適宜修正
+        if (document.activeElement == null) return
+        if (document.activeElement.nodeName.toLowerCase() != 'div') return
+        if (document.activeElement.children == null) return
+        if (document.activeElement.children.length == 0) return
+        var p = document.activeElement.children[0]
+        if (p.nodeName.toLowerCase() != 'p') return
+        function inputText(text) {
+            p.innerText = text
+        }
+
+        var now = new Date()
+        switch (direction) {
+            case kCurrent():
+                break
+            case kForward():
+                now.setMinutes(now.getMinutes() + 14)
+                now.setMinutes(Math.floor(now.getMinutes() / 15) * 15)
+                break
+            case kBackward():
+                now.setMinutes(Math.floor(now.getMinutes() / 15) * 15)
+                break
+        }
+
+        var hourText = ('00' + now.getHours()).slice(-2)
+        var minuteText = ('00' + now.getMinutes()).slice(-2)
+        inputText(text.replace('%TIME%', hourText + ':' + minuteText))
+    }
+    var direction = kCurrent()
+    if (e.shiftKey && !e.ctrlKey) direction = kForward()
+    if (!e.shiftKey && e.ctrlKey) direction = kBackward()
+    if (e.altKey && e.code == 'Digit1') input('業務開始 %TIME%', direction)
+    if (e.altKey && e.code == 'Digit2') input('休憩開始 %TIME%', direction)
+    if (e.altKey && e.code == 'Digit3') input('休憩終了 %TIME%', direction)
+    if (e.altKey && e.code == 'Digit4') input('業務終了 %TIME%', direction)
+}
